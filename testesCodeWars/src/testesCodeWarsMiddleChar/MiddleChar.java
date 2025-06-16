15ae5534327d8c3541fb1cc4d19eff73b14c2b86
Gabriel Lima <106500760+gabriellima-end@users.noreply.github.com>
2025-06-16 10:26:30 -0300

    Add files via upload

--- testesCodeWars/src/testesCodeWarsMiddleChar/MiddleChar.java
+++ testesCodeWars/src/testesCodeWarsMiddleChar/MiddleChar.java
@@ -1,20 +1,69 @@
 package testesCodeWarsMiddleChar;
 
+import java.util.Arrays;
+
 public class MiddleChar {
 
 	public static void main(String[] args) {
 		
-		String sentence = "test";
+		String sentence = "middle";
 		
-		String[] splitSentence = sentence.split("");
+		System.out.println(getMiddle(sentence));
 		
-		for()
-		
+//		String[] splitSentence = sentence.split("");
+//		
+//		int numLen = splitSentence.length;
+//		
+//		int middleNum = numLen / 2;
+//		
+//		String middleLetter = "";
+//		
+//		if(numLen %2 == 0) {
+//			
+//			middleLetter = splitSentence[middleNum-1] + splitSentence[middleNum];
+//			
+//			System.out.println(middleLetter);
+//			
+//		}else {
+//		
+//			for(int i = 0; i<numLen; i++) {
+//				
+//				middleLetter = splitSentence[middleNum];
+//				
+//			}
+//			
+//			System.out.println(middleLetter);
+//		}
+//		
 	}
 	
 	public static String getMiddle(String word) {
 	    
+		String[] splitSentence = word.split("");
 		
-		return "";
+		int numLen = splitSentence.length;
+		
+		int middleNum = numLen / 2;
+		
+		String middleLetter = "";
+		
+		if(numLen %2 == 0) {
+			
+			middleLetter = splitSentence[middleNum-1] + splitSentence[middleNum];
+			
+			return middleLetter;
+			
+		}else {
+		
+			for(int i = 0; i<numLen; i++) {
+				
+				middleLetter = splitSentence[middleNum];
+				
+			}
+			
+			return middleLetter;
+		}
+		
+		
 	  }
 }
